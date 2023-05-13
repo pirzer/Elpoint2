a410acb2f6a1463ffe0f2e02f9b546a4435d3c31
pirzer <pirzer1@yahoo.com>
2023-05-13 22:47:21 +0000

    changing debug from true to false and checking the heroku deployment

--- elpoint2/settings.py
+++ elpoint2/settings.py
@@ -30,7 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# To change Debug from True to False before heroku deployment
+DEBUG = False
 
 ALLOWED_HOSTS = ['elpoint2.herokuapp.com', 'localhost']
 
